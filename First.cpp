5393bdb809caf5b6718fff249b228482f3b64222
Sahil <sahilp5663@gmail.com>
2026-06-03 11:30:58 +0530

    My c++ code of 3 days learning

---
+++ First.cpp
@@ -0,0 +1,129 @@
+#include <iostream>   // header file
+using namespace std; // for using data types like cout cin..
+#include <string> // for using string data type
+
+int main() { // main function is the entry point of the program
+
+   // first code in c++ is to print hello world on the screen
+
+    // std::cout << " hello bro sahil" << std::endl;
+
+          // printing 
+    // int a = 18;
+    // cout << "My age is:" << a << endl;
+    // int b = 20;
+    // cout << "the sum of a and b is :" << a+b<< endl;
+
+         // all data types in c++
+    // float a = 2.4;
+    // cout << a << endl;
+    // double b = 3.5657345;
+    // cout << b << endl; 
+    // int c = 200;
+    // cout << c << endl;
+    // char alphabet = 's';
+    // cout << alphabet << endl;
+    // bool iwillgetjob = true;
+    // cout << iwillgetjob << endl;
+    // bool iwillnotgetjob = false;
+    // cout << iwillnotgetjob << endl;
+    // bool sheisbeautiful = 1;
+    // cout << sheisbeautiful << endl;
+    // bool heishandsome =0;
+    // cout << heishandsome << endl;
+    
+
+        //  char name[10]; I used array to store string data type but it is not recommended in c++ 
+
+        //  cout << "what is your Name ?" << endl;
+
+        //   cin>>name;
+        //   cout << "your name is "<< name << endl;
+     
+
+        //          string name; 
+        //  cout << "what is your Name ?" << endl;
+
+        //   cin>>name;
+        //   cout << "your name is "<< name << endl;
+
+
+      //   int age;
+      //   cout << "what is your age ?" << endl;
+
+      //   cin>>age;
+
+      //    if (age>=18){
+      //       cout<< "you are eligible for voting"<< endl;
+      //    } else {
+      //       cout << " you are not eligible for voting"<< endl;
+      //    }
+
+           
+            //    int marks;
+            //    cout << "What are your marks ?" << endl;
+
+            //    cin >> marks;
+
+            //  switch(marks){
+            //    case 12:
+            //    cout<<"you are fail"<< endl;
+            //    break;
+            //    case 33: 
+            //    cout<< "you are passed"<< endl;
+            //    break;
+            //         default:
+            //         cout<< "invalid marks"<< endl;
+
+            //  }
+
+
+        //        int marks;
+        //        cout<< "what are your marks ?" << endl;
+        //        cin >> marks;
+
+              // ternary operator is a short hand of if else statement
+
+        // (marks > 80) ? cout << " you are passed with  good rank": cout << "Principal will handle you";
+
+            // for loop is used to repeat a block of code for a certain number of times
+
+            // for (int repeat =1; repeat<=10;repeat = repeat+1){
+            //     cout<< "I love you "  << endl;
+            //     if (repeat==5){
+            //         break; break statement is used to exit the loop when a certain condition is met
+            //     }
+            // }
+
+
+        //  for (int repeat =1; repeat<=10;repeat = repeat+1){
+                
+        //    if (repeat==5){
+        //             break;
+        //         }
+        //         cout<< "I love you "  << endl;
+        //     }
+           
+        //   for ( int a =1 ; a<=10 ; a= a+1){
+            
+        //     if (a==5){
+        //         continue; continue statement is used to skip the current iteration of the loop when a certain condition is met
+        //     }
+        //      cout << a << endl;
+        //   }
+                
+             //do while loop is used to repeat a block of code at least once and then check the condition 
+             
+            //   int a= 1;
+            //   while ( a <=50){ 
+                
+            //     cout << a << endl;
+            //     a =a+1; 
+
+
+            //   }
+
+
+               return 0;
+    
+} 
